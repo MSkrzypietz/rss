37c4383c930407d3ff64745e3239d2ef94c4c96b
MSkrzypietz <michael.skr97@gmail.com>
2024-08-14 12:05:30 +0200

    Add http to https redirection

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/joho/godotenv"
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 	"log"
+	"net"
 	"net/http"
 	"os"
 )
@@ -19,9 +20,14 @@ func main() {
 		}
 	}
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatalln("PORT is undefined")
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		log.Fatalln("HTTP_PORT is undefined")
+	}
+
+	httpsPort := os.Getenv("HTTPS_PORT")
+	if httpsPort == "" {
+		log.Fatalln("HTTPS_PORT is undefined")
 	}
 
 	dbURL := os.Getenv("DB_URL")
@@ -44,14 +50,10 @@ func main() {
 	go apiCfg.ContinuousFeedScraping()
 
 	mux := http.NewServeMux()
-
 	mux.Handle("/", renderCfg.Handlers())
 	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", apiCfg.Handlers()))
-
 	corsMux := middlewareCors(mux)
 
-	server := http.Server{Addr: ":" + port, Handler: corsMux}
-	fmt.Printf("Serving on port: %s\n", port)
 	if isProductionEnv() {
 		certFile := os.Getenv("CERT_FILE_PATH")
 		if certFile == "" {
@@ -63,12 +65,32 @@ func main() {
 			log.Fatalln("KEY_FILE_PATH is undefined")
 		}
 
+		go redirectHttpToHttps(httpPort, httpsPort)
+		server := http.Server{Addr: ":" + httpsPort, Handler: corsMux}
+		fmt.Printf("Serving on https port: %s\n", httpsPort)
 		log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
 	} else {
+		server := http.Server{Addr: ":" + httpPort, Handler: corsMux}
+		fmt.Printf("Serving on http port: %s\n", httpPort)
 		log.Fatal(server.ListenAndServe())
 	}
 }
 
 func isProductionEnv() bool {
 	return os.Getenv("APP_ENV") == "production"
+}
+
+func redirectHttpToHttps(httpPort, httpsPort string) {
+	server := http.Server{
+		Addr: ":" + httpPort,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			host, _, _ := net.SplitHostPort(r.Host)
+			redirectUrl := r.URL
+			redirectUrl.Host = net.JoinHostPort(host, httpsPort)
+			redirectUrl.Scheme = "https"
+			http.Redirect(w, r, redirectUrl.String(), http.StatusMovedPermanently)
+		}),
+	}
+	fmt.Printf("Redirecting http traffic from port %s to %s\n", httpPort, httpsPort)
+	log.Println(server.ListenAndServe())
 }
