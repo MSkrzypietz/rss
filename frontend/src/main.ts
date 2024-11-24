8616c8d4d5d230d2bfbff7a21cf6b12f53d1c6ea
MSkrzypietz <michael.skr97@gmail.com>
2024-11-24 16:57:45 +0100

    Add sit-onyx

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,19 @@
 import './assets/main.css';
+import '@fontsource-variable/source-code-pro';
+import '@fontsource-variable/source-sans-3';
+import 'sit-onyx/style.css';
 
 import { createApp } from 'vue';
+import { createOnyx } from 'sit-onyx';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
 
+const onyx = createOnyx();
 const app = createApp(App);
 
+app.use(onyx);
 app.use(createPinia());
 app.use(router);
 
