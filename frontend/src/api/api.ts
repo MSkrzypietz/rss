358df9ebde83ebec1bcf314eb6434a5fc0b6b0a5
MSkrzypietz <michael.skr97@gmail.com>
2025-01-06 15:17:09 +0100

    Add api url via env variable

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = import.meta.env.PROD ? '' : 'http://localhost:8080';
+const baseUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : 'http://localhost:8080';
 
 const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
