1bbbb505eab685b6098b0186e9cb9b49cd986bdb
MSkrzypietz <michael.skr97@gmail.com>
2025-01-08 22:52:13 +0100

    Fix cors settings

--- backend/cors.go
+++ backend/cors.go
@@ -1,12 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func middlewareCors(next http.Handler) http.Handler {
+	origin := os.Getenv("ORIGIN_URL")
+	if origin == "" {
+		origin = "*"
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
