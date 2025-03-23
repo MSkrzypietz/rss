9e09a06d619843d79ea5bdb78634efb72245fb46
MSkrzypietz <michael.skr97@gmail.com>
2025-03-23 12:19:38 +0100

    Silently ignore unique constraint errors for new post_reads entries

--- backend/internal/database/post_reads.sql.go
+++ backend/internal/database/post_reads.sql.go
@@ -12,6 +12,7 @@ import (
 const createPostRead = `-- name: CreatePostRead :one
 INSERT INTO post_reads (user_id, post_id)
 VALUES (?, ?)
+ON CONFLICT(user_id, post_id) DO UPDATE SET user_id=post_reads.user_id, post_id=post_reads.post_id
     RETURNING id, created_at, updated_at, user_id, post_id
 `
 
--- backend/sql/queries/post_reads.sql
+++ backend/sql/queries/post_reads.sql
@@ -1,5 +1,6 @@
 -- name: CreatePostRead :one
 INSERT INTO post_reads (user_id, post_id)
 VALUES (?, ?)
+ON CONFLICT(user_id, post_id) DO UPDATE SET user_id=post_reads.user_id, post_id=post_reads.post_id
     RETURNING *;
 
