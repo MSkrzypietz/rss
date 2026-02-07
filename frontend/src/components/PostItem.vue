c23f6e237b0f3102c79c59f1bf26c78eea8031ef
MSkrzypietz <michael.skr97@gmail.com>
2026-02-07 17:48:06 +0100

    truncate long post descriptions

--- frontend/src/components/PostItem.vue
+++ frontend/src/components/PostItem.vue
@@ -2,6 +2,7 @@
 import { type Post } from '@/api/posts.ts';
 import { OnyxHeadline, OnyxButton } from 'sit-onyx';
 import { usePostStore } from '@/stores/post.ts';
+import { computed } from 'vue';
 
 const postStore = usePostStore();
 
@@ -9,14 +10,25 @@ const props = defineProps<{
   post: Post;
 }>();
 
-let publishDate: string | null = null;
-if (props.post.published_at !== null) {
+const publishDate = computed(() => {
+  if (props.post.published_at === null) {
+    return null;
+  }
   const addLeadingZero = (n: number) => n.toString().padStart(2, '0');
   const d = new Date(props.post.published_at);
   const date = `${addLeadingZero(d.getDate())}.${addLeadingZero(d.getMonth() + 1)}.${d.getFullYear()}`;
   const time = `${addLeadingZero(d.getHours())}:${addLeadingZero(d.getMinutes())}:${addLeadingZero(d.getSeconds())}`;
-  publishDate = `${date} ${time}`;
-}
+  return `${date} ${time}`;
+});
+
+const descriptionLength = 500;
+const description = computed(() => {
+  const desc = props.post.description.trim();
+  if (desc.length <= descriptionLength) {
+    return desc;
+  }
+  return desc.slice(0, descriptionLength) + '...';
+});
 </script>
 
 <template>
@@ -26,7 +38,7 @@ if (props.post.published_at !== null) {
       <OnyxButton label="Read" density="compact" @click.stop.prevent="postStore.markPostAsRead(post.id)"></OnyxButton>
     </div>
     <div class="post-item__content">
-      <span class="onyx-text">{{ post.description }}</span>
+      <span class="onyx-text">{{ description }}</span>
     </div>
     <p class="onyx-text">{{ publishDate }} - {{ post.feed_name }}</p>
   </a>
